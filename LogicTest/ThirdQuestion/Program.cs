cdfd36881d385dc456dd463693f01f431a44be57
Willian Daniel <will.ge.dan@gmail.com>
2024-10-08 08:57:58 -0300

    Added the third question of the programming logic test

---
+++ LogicTest/ThirdQuestion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ThirdQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Construa a tabuada de um número. (ex: 1x1=1, 1x2=2,...,1x10=10);
+            Console.Write("Enter a number for the multiplication table: ");
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number}x{i}={number*i}");
+            }
+        }
+    }
+}
